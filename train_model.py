9fb16c229cef686b257c3cb9b9d3ef59a0d36b84
Betty <brmtengwa@gmail.com>
2024-08-11 23:15:59 +0000

    project update

---
+++ train_model.py
@@ -0,0 +1,186 @@
+#TODO: Import your dependencies.
+#For instance, below are some dependencies you might need if you are using Pytorch
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+
+import argparse
+
+
+#TODO: Import dependencies for Debugging andd Profiling
+
+def test(model, test_loader):
+    '''
+    TODO: Complete this function that can take a model and a 
+          testing data loader and will get the test accuray/loss of the model
+          Remember to include any debugging/profiling hooks that you might need
+    '''
+    model.eval()
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data = data.view(data.shape[0], -1)
+            output = model(data)
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    print(f'Test set: Accuracy: {correct}/{len(test_loader.dataset)} = {100*(correct/len(test_loader.dataset))}%)')
+    
+def train(model, train_loader, cost, optimizer, epoch):
+    '''
+    TODO: Complete this function that can take a model and
+          data loaders for training and will get train the model
+          Remember to include any debugging/profiling hooks that you might need
+    '''
+    model.train()
+    for e in range(epoch):
+        running_loss=0
+        correct=0
+        for data, target in train_loader:
+            data = data.view(data.shape[0], -1)
+            optimizer.zero_grad()
+            pred = model(data)
+            loss = cost(pred, target)
+            running_loss+=loss
+            loss.backward()
+            optimizer.step()
+            pred=pred.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+        print(f"Epoch {e}: Loss {running_loss/len(train_loader.dataset)}, Accuracy {100*(correct/len(train_loader.dataset))}%")
+
+def net():
+    '''
+    TODO: Complete this function that initializes your model
+          Remember to use a pretrained model
+    '''
+    model = models.resnet50(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    num_features = model.fc.in_features
+    model.fc = nn.Sequential(
+                   nn.Linear(num_features, 128),
+                   nn.ReLU(inplace=True),
+                   nn.Linear(128, 133))
+    return model
+    pass
+
+def create_data_loaders(data, batch_size):
+    '''
+    This is an optional function that you may or may not need to implement
+    depending on whether you need to use data loaders or not
+    '''
+    train_data_path = os.path.join(data, 'train')
+    test_data_path = os.path.join(data, 'test')
+    validation_data_path = os.path.join(data, 'valid')
+
+    training_transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+testing_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+
+    train_data = torchvision.datasets.ImageFolder(
+        root=train_data_path,
+        transform=train_transform
+    )
+    train_data_loader = torch.utils.data.DataLoader(
+        train_data,
+        batch_size=batch_size,
+        shuffle=True,
+    )
+
+    test_data = torchvision.datasets.ImageFolder(
+        root=test_data_path,
+        transform=test_transform
+    )
+    test_data_loader = torch.utils.data.DataLoader(
+        test_data,
+        batch_size=batch_size,
+        shuffle=False,
+    )
+
+    validation_data = torchvision.datasets.ImageFolder(
+        root=validation_data_path,
+        transform=test_transform,
+    )
+    validation_data_loader = torch.utils.data.DataLoader(
+        validation_data,
+        batch_size=batch_size,
+        shuffle=False,
+    )
+
+    return train_data_loader, test_data_loader, validation_data_loader
+def main(args):
+    '''
+    TODO: Initialize a model by calling the net function
+    '''
+    logger.info(f'Hyperparameters are LR: {args.learning_rate}, Batch Size: {args.batch_size}, Early Stopping:               {args.early_stopping_rounds}')
+    logger.info(f'Data Paths: {args.data}')
+
+    model=net()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    criterion = nn.CrossEntropyLoss(ignore_index=133)
+    optimizer = optim.Adam(model.fc.parameters(), lr=args.learning_rate)
+    train_loader, test_loader, validation_loader = create_data_loaders(args.data,
+                                                                       args.batch_size)
+    model = model.to(device)
+    hook = smd.Hook.create_from_json_file()
+    hook.register_hook(model)
+    
+    logger.info("Training the model")
+    tic = time.perf_counter()
+    model = train(model,
+                  train_loader,
+                  validation_loader,
+                  criterion,
+                  optimizer,
+                  device,
+                  hook,
+                  early_stopping=args.early_stopping_rounds)
+    toc = time.perf_counter()
+    logger.info(f"Training took {toc - tic:0.4f} seconds")
+
+    logger.info("Testing the model")
+    test(model, test_loader, criterion, device, hook)
+
+    logger.info("Saving the model")
+    torch.save(model.state_dict(), os.path.join(args.model_dir, "model.pth"))
+
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--learning-rate',
+                        type=float,
+                        default=0.001)
+    parser.add_argument('--batch-size',
+                        type=int,
+                        default=32)
+    parser.add_argument('--early-stopping-rounds',
+                        type=int,
+                        default=10)
+    parser.add_argument('--data', type=str,
+                        default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--model_dir',
+                        type=str,
+                        default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--output_dir',
+                        type=str,
+                        default=os.environ['SM_OUTPUT_DATA_DIR'])
+
+    args = parser.parse_args()
+    print(args)
+
+    main(args)
+    
